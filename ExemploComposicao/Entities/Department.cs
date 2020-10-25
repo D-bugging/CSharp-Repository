d83e02e121ee863d6520341f2a481758ca27a852
D-bugging <archmelo@protonmail.com>
2020-10-25 00:58:27 -0300

    Criação do diretório exemplo ExemploComposicao

---
+++ ExemploComposicao/Entities/Department.cs
@@ -0,0 +1,14 @@
+namespace ExemploComposicao.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+        public Department()
+        {
+        }
+        public Department (string name)
+        {
+            Name = name;
+        }
+    }
+}
